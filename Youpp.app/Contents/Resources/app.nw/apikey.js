dfa0b9dcc4f31fc2412b135b2ae43d2e08380412
Ambrose Chua <ambrose.chua.cm@gmail.com>
2014-03-07 21:51:43 +0800

    First commit

---
+++ Youpp.app/Contents/Resources/app.nw/apikey.js
@@ -0,0 +1,3 @@
+module.exports = {
+	key: "AIzaSyD88ejeWUIVWmU7j5lhZNLyP9q3S6TFtFQ"
+};
---
+++ pack-Youpp.bat
@@ -0,0 +1,5 @@
+xcopy /s node-webkit Youpp
+cd Youpp
+copy /b nw.exe+Youpp.nw Youpp.exe
+del nw.exe
+del nwsnapshot.exe
